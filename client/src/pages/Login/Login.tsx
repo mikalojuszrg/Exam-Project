635169b9574aad71839be46f8959dd03781cee82
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-29 22:16:23 +0300

    created all elements of login page

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,5 +1,24 @@
+import LoginForm from "./LoginForm/LoginForm";
+import image from "../Login/assets/people.jpg";
+import styles from "./Login.module.scss";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <main className={styles.container}>
+      <section className={styles.container__info}>
+        <h2 className={styles.container__subheading}>NEW PLATFORM</h2>
+        <h1 className={styles.container__heading}>Your Next Social Life.</h1>
+        <p className={styles.container__description}>
+          Discover a new social media experience. Your interests matter, and so
+          does your time. Join now and get more of what you care about.
+        </p>
+        <LoginForm />
+      </section>
+      <section className={styles.container__visual}>
+        <img src={image} alt="people talking" />
+      </section>
+    </main>
+  );
 };
 
 export default Login;
