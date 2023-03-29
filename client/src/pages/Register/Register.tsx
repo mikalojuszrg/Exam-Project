6b15e5bb4e03d6df3827859b1ffc15330fa83c3d
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-29 22:16:49 +0300

    created register form

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,5 +1,11 @@
+import RegisterForm from "./RegisterForm/RegisterForm";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default Register;
