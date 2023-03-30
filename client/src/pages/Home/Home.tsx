516699ec64b2b1456e9187ba37b61e4b54d80e74
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-30 20:33:11 +0300

    home page updated with new components

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,5 +1,13 @@
+import QuestionForm from "./QuestionForm/QuestionForm";
+import TimeLine from "./Timeline/Timeline";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <QuestionForm />
+      <TimeLine />
+    </div>
+  );
 };
 
 export default Home;
