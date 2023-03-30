0e4cc0ea53a48c403edc5924d292264be735f876
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-30 21:54:35 +0300

    question page updated

--- client/src/pages/Question/Question.tsx
+++ client/src/pages/Question/Question.tsx
@@ -1,8 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useQuestion } from "../../hooks/question";
+
+type QuestionPreview = {
+  title: string;
+  content: string;
+};
 
 const Question = () => {
-  const { id } = useParams();
-  return <div>Question{id}</div>;
+  const { id } = useParams<{ id: string }>();
+  const { data, isLoading } = useQuestion(parseInt(id || "0"));
+  const question: QuestionPreview = data || { title: "", content: "" };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{question.title}</h1>
+      <p>{question.content}</p>
+    </div>
+  );
 };
 
 export default Question;
