286c8beb79103c4cc56fab406f497c7ead967b42
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-01 12:44:07 +0300

    updated util func

--- client/src/utils/reverseArray.ts
+++ client/src/utils/reverseArray.ts
@@ -1,5 +1,6 @@
+import { Answer } from "../types/answer";
 import { Question } from "../types/question";
 
-export const reverseArray = (arr: Question[]) => {
+export const reverseArray = (arr: Array<Question | Answer>) => {
   return [...arr].reverse();
 };
