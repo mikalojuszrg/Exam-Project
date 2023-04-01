ec44f5e304631caf10ac126b44a1abbdf4210658
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-01 11:27:54 +0300

    answer api hooks for query

---
+++ client/src/hooks/answer.ts
@@ -0,0 +1,34 @@
+import { Answer, NewAnswer } from "../types/answer";
+import {
+  createAnswer,
+  deleteAnswer,
+  fetchAnswers,
+  updateAnswer,
+} from "../api/answer";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+const ANSWERS = "ANSWERS";
+
+export const useCreateAnswer = () => {
+  return useMutation((data: { questionId: number; answer: NewAnswer }) =>
+    createAnswer(data.questionId, data.answer)
+  );
+};
+
+export const useAnswers = (questionId: number) => {
+  return useQuery([ANSWERS, questionId], () => fetchAnswers(questionId));
+};
+
+export const useDeleteAnswer = () => {
+  return useMutation((ids: [number, string]) => deleteAnswer(ids[0], ids[1]));
+};
+
+export const useUpdateAnswer = () => {
+  return useMutation(
+    (updatedAnswer: Answer) =>
+      updateAnswer(updatedAnswer.question_id, updatedAnswer._id, updatedAnswer),
+    {
+      mutationKey: [ANSWERS, "updateAnswer"],
+    }
+  );
+};
