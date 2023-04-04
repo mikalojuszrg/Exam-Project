0d046409a310ae408ed7aa0a5da425fa13c79891
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-04 09:27:27 +0300

    fixed question sorting

--- client/src/pages/Home/Timeline/QuestionTimeline.tsx
+++ client/src/pages/Home/Timeline/QuestionTimeline.tsx
@@ -1,35 +1,34 @@
+import {
+  sortQuestionsByAnswersAsc,
+  sortQuestionsByAnswersDesc,
+  sortQuestionsByDateAsc,
+  sortQuestionsByDateDesc,
+} from "../../../utils/sortQuestions";
+
 import Loader from "../../../components/Loader/Loader";
 import { Question } from "../../../types/question";
 import QuestionCard from "../../../components/QuestionCard/QuestionCard";
-import { reverseArray } from "../../../utils/reverseArray";
-import { sortQuestions } from "../../../utils/sortQuestions";
 import styles from "./QuestionTimeline.module.scss";
 import { useQuestions } from "../../../hooks/question";
 import { useState } from "react";
 
-const QuestionTimeLine = () => {
+const QuestionTimeline = () => {
   const { data, isLoading } = useQuestions();
   const [sortBy, setSortBy] = useState("");
   const [sortOrder, setSortOrder] = useState("");
   const [filter, setFilter] = useState("all");
+
   const questions = data || [];
-  const reversedQuestions = reverseArray(questions);
-  const sortedQuestions = sortQuestions(reversedQuestions, sortBy, sortOrder);
+  const sortedQuestionsByDateAsc = sortQuestionsByDateAsc(questions);
+  const sortedQuestionsByDateDesc = sortQuestionsByDateDesc(questions);
+  const sortedQuestionsByAnswersAsc = sortQuestionsByAnswersAsc(questions);
+  const sortedQuestionsByAnswersDesc = sortQuestionsByAnswersDesc(questions);
 
-  const handleSortByDate = () => {
-    if (sortBy === "date") {
+  const handleSortBy = (sortType: string) => {
+    if (sortBy === sortType) {
       setSortOrder(sortOrder === "asc" ? "desc" : "asc");
     } else {
-      setSortBy("date");
-      setSortOrder("asc");
-    }
-  };
-
-  const handleSortByAnswers = () => {
-    if (sortBy === "answers") {
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
-    } else {
-      setSortBy("answers");
+      setSortBy(sortType);
       setSortOrder("asc");
     }
   };
@@ -37,29 +36,48 @@ const QuestionTimeLine = () => {
   const handleFilterQuestions = () => {
     switch (filter) {
       case "answered":
-        return sortedQuestions.filter(
-          (question) => question.answerCount ?? 0 > 0
-        );
+        return sortBy === "answers"
+          ? sortedQuestionsByAnswersDesc.filter(
+              (question) => question.answerCount ?? 0 > 0
+            )
+          : sortedQuestionsByDateDesc.filter(
+              (question) => question.answerCount ?? 0 > 0
+            );
       case "unanswered":
-        return sortedQuestions.filter(
-          (question) => (question.answerCount ?? 0) === 0
-        );
+        return sortBy === "answers"
+          ? sortedQuestionsByAnswersAsc.filter(
+              (question) => (question.answerCount ?? 0) === 0
+            )
+          : sortedQuestionsByDateAsc.filter(
+              (question) => (question.answerCount ?? 0) === 0
+            );
       default:
-        return sortedQuestions;
+        return sortBy === "answers"
+          ? sortedQuestionsByAnswersDesc
+          : sortedQuestionsByDateDesc;
     }
   };
 
   const filteredQuestions = handleFilterQuestions();
 
-  // Apply reverse sorting if sortOrder is "desc"
   const finalQuestions =
-    sortOrder === "desc" ? reverseArray(filteredQuestions) : filteredQuestions;
+    sortOrder === "desc" ? filteredQuestions.reverse() : filteredQuestions;
 
   return (
     <div className={styles.container}>
       <div className={styles.sortButtons}>
-        <button onClick={handleSortByDate}>Sort by date</button>
-        <button onClick={handleSortByAnswers}>Sort by answers</button>
+        <button onClick={() => handleSortBy("date-asc")}>
+          Sort by date (asc)
+        </button>
+        <button onClick={() => handleSortBy("date-desc")}>
+          Sort by date (desc)
+        </button>
+        <button onClick={() => handleSortBy("answers-desc")}>
+          Sort by answer count (desc)
+        </button>
+        <button onClick={() => handleSortBy("answers-asc")}>
+          Sort by answer count (asc)
+        </button>
       </div>
       <div className={styles.filterButtons}>
         <button onClick={() => setFilter("all")}>All questions</button>
@@ -81,4 +99,4 @@ const QuestionTimeLine = () => {
   );
 };
 
-export default QuestionTimeLine;
+export default QuestionTimeline;
--- client/src/utils/sortQuestions.ts
+++ client/src/utils/sortQuestions.ts
@@ -1,43 +1,41 @@
 import { Question } from "../types/question";
 
-export const sortQuestions = (
-  questions: Question[],
-  sortBy: string,
-  sortOrder: string
-) => {
+export const sortQuestionsByDateAsc = (questions: Question[]) => {
   const sortedQuestions = [...questions];
-  let ascending = sortOrder === "asc";
-  switch (sortBy) {
-    case "answers":
-      sortedQuestions.sort((a, b) => {
-        const answersA = a.answersCount || 0;
-        const answersB = b.answersCount || 0;
-        if (ascending) {
-          return answersA - answersB;
-        } else {
-          return answersB - answersA;
-        }
-      });
-      break;
-    case "date":
-      sortedQuestions.sort((a, b) => {
-        const dateA = new Date(a.date);
-        const dateB = new Date(b.date);
-        if (ascending) {
-          return dateA.getTime() - dateB.getTime();
-        } else {
-          return dateB.getTime() - dateA.getTime();
-        }
-      });
-      ascending = !ascending; // toggle sort order
-      break;
-    default:
-      sortedQuestions.sort((a, b) => {
-        const dateA = new Date(a.date);
-        const dateB = new Date(b.date);
-        return dateB.getTime() - dateA.getTime();
-      });
-      break;
-  }
+  sortedQuestions.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateA.getTime() - dateB.getTime();
+  });
+  return sortedQuestions;
+};
+
+export const sortQuestionsByDateDesc = (questions: Question[]) => {
+  const sortedQuestions = [...questions];
+  sortedQuestions.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB.getTime() - dateA.getTime();
+  });
+  return sortedQuestions;
+};
+
+export const sortQuestionsByAnswersDesc = (questions: Question[]) => {
+  const sortedQuestions = [...questions];
+  sortedQuestions.sort((a, b) => {
+    const answersA = a.answerCount || 0;
+    const answersB = b.answerCount || 0;
+    return answersB - answersA;
+  });
+  return sortedQuestions;
+};
+
+export const sortQuestionsByAnswersAsc = (questions: Question[]) => {
+  const sortedQuestions = [...questions];
+  sortedQuestions.sort((a, b) => {
+    const answersA = a.answerCount || 0;
+    const answersB = b.answerCount || 0;
+    return answersA - answersB;
+  });
   return sortedQuestions;
 };
