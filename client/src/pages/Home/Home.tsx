229ee1169c4af77a6ba6492a12f09e3d29669ae0
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-04 20:22:03 +0300

    added form toggle

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,22 @@
+import Button from "../../components/Button/Button";
 import QuestionForm from "./QuestionForm/QuestionForm";
 import QuestionTimeLine from "./Timeline/QuestionTimeline";
+import styles from "./Home.module.scss";
+import { useState } from "react";
 
 const Home = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const toggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
   return (
-    <div>
-      <QuestionForm />
+    <div className={styles.container}>
+      <Button variant="primary" onClick={toggleForm}>
+        {showForm ? "Hide" : "Show"} Form
+      </Button>
+      {showForm && <QuestionForm />}
       <QuestionTimeLine />
     </div>
   );
