0727866ad9de31da598c5cf1fb4e5b8c03f0bb02
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-04 11:58:23 +0300

    added navbar and footer

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,7 +1,15 @@
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
 import { PropsWithChildren } from "react";
 
 const MainLayout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
