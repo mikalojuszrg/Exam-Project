b5b409f3bebb9faf5617df05a6a575a5018b2f51
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-04-05 18:26:54 +0300

    removed commented code

--- client/src/utils/reverseArray.ts
+++ client/src/utils/reverseArray.ts
@@ -1,11 +1,3 @@
-// import { Answer } from "../types/answer";
-// import { Question } from "../types/question";
-
-// export const reverseArray = (arr: Array<Question | Answer>) => {
-//   return [...arr].reverse();
-// };
-
-import { Answer } from "../types/answer";
 import { Question } from "../types/question";
 
 export const reverseArray = (arr: Question[]) => {
